3bfa436a807f629c687350931659f82aa5b55c13
Balwinder Sodhi <sodhi@iitrpr.ac.in>
2022-02-03 16:03:17 +0530

    Initial version

---
+++ app/src/main/java/org/cs305/ExcelProcessor.java
@@ -0,0 +1,35 @@
+package org.cs305;
+
+/**
+ *
+ * @author someuser
+ */
+public class ExcelProcessor {
+    
+    private String firstFilePath;
+    private String secondFilePath;
+    private int locationColInFirst;
+    private int locationColInSecond;
+
+    public ExcelProcessor(String firstFilePath, String secondFilePath, 
+            int locationColInFirst, int locationColInSecond) {
+        this.firstFilePath = firstFilePath;
+        this.secondFilePath = secondFilePath;
+        this.locationColInFirst = locationColInFirst;
+        this.locationColInSecond = locationColInSecond;
+    }
+    
+    
+    
+    public void compareFiles() {
+        // Read the first file's rows
+        
+        // Read 2nd file rows
+        
+        // Compare using the necessary logic as discussed
+        
+        // Create a copy of the first input file
+        
+        // Write the output to a copy of the first file.
+    }
+}
